7bcd65560b1b38bba123acddd2b9da71c1d3401a
pepe <pepe@MacBook-Pro-3.local>
2020-07-13 15:00:15 +0900

    APIサーバー実装

---
+++ src/app.js
@@ -0,0 +1,100 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+const port = 3001
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+app.use(bodyParser.json())
+app.use((res, req, next) => {
+    res.header('Content-Type', 'application/json; charset=utf-8')
+    next()
+})
+
+const mysql = require('mysql')
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'ns_user',
+    password: 'sM5T$e7D4',
+    database: 'todolist'
+})
+
+// con.connect(function(err) {
+//     if (err) throw err;
+//     console.log('Connected');
+// })
+
+// ヘルスチェック
+app.get('/', (req, res) => res.send('Hello World!'))
+
+// 一覧取得
+app.get('/api/task', (req, res) => { 
+    const sql = 'SELECT * FROM task ORDER BY orderno'
+
+    con.query(sql, function (err, result, fields) {  
+        if (err) throw err
+        console.log(result)
+
+        res.send(result)
+    })
+})
+
+// 追加
+app.post('/api/task', (req, res) => {
+    const sql = 'INSERT INTO task SET ?'
+
+    con.query(sql, req.body, (err, result, fields) => {
+        if (err) throw err
+        console.log(result)
+
+        res.send()
+    })
+})
+
+// 並び替え
+app.post('/api/task/sort', (req, res) => {
+    
+    const sortArr = req.body.param.map((task) => {
+        return {
+            id: task.id,
+            params: {
+                orderno: task.orderno
+            }
+        }
+    })
+
+    for (const sortDict of sortArr) {
+        const sql = `UPDATE task SET ? WHERE id = ${sortDict.id}`
+        con.query(sql, sortDict.params, (err, result, fields) => {
+            if (err) throw err
+            console.log(result)
+        })
+    }
+    res.send()
+})
+
+// 更新
+app.put('/api/task/:id', (req, res) => {
+    const sql = `UPDATE task SET ? WHERE id = ${req.params.id}` 
+    con.query(sql, req.body, (err, result, fields) => {
+        if (err) throw err
+        console.log(result)
+
+        res.send()
+    })
+})
+
+// 削除
+app.delete('/api/task/:id', (req, res) => {
+    const sql = `DELETE FROM task WHERE id = ${req.params.id}`
+
+    con.query(sql, (err, result, fields) => {
+        if (err) throw err
+        console.log(result)
+
+        res.send()
+    })
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
